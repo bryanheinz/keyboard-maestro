f30ddb4fc4cdcfb941d8496af6dfc13375415a57
Bryan Heinz <git@bryanheinz.com>
2021-02-04 17:47:09 +0000

    new km

---
+++ text-manipulation/wpEnlightenCode.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python3
+
+"""
+This file is a basic Keyboard Maestro Python template script. It takes selected
+text input and prints it back out to KM.
+"""
+
+import syslog
+from sys import stdin
+
+
+def logd(msg):
+    syslog.syslog(syslog.LOG_ALERT, "[km] {}".format(msg))
+
+def main(selection):
+    txt = "<!-- wp:enlighter/codeblock -->\n<pre class="EnlighterJSRAW" data-enlighter-language="generic" data-enlighter-theme="" data-enlighter-highlight="" data-enlighter-linenumbers="" data-enlighter-lineoffset="" data-enlighter-title="" data-enlighter-group="">{}</pre>\n<!-- /wp:enlighter/codeblock -->"
+    print(txt)
+
+selection = stdin.read()
+
+try:
+    main(selection)
+except Exception as e:
+    logd(e)
+    exit(1)

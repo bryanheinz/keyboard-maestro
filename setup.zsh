4375b868d38c1ad33067c4ff5fce30ca0a89ea37
Bryan Heinz <git@bryanheinz.com>
2021-03-30 18:16:29 +0000

    uncomplicated setup script

--- setup.zsh
+++ setup.zsh
@@ -1,18 +1,10 @@
 #!/bin/zsh
 
+baseDir=$(dirname "$0")
+kmDir="/usr/local/keyboard-maestro"
 theUser=$(/usr/bin/stat -f "%Su" /dev/console)
 
-if [[ ! -f /Library/Developer/CommandLineTools/usr/bin/git ]]; then
-    echo "git doesn't appear to be installed."
-    echo "manually install from https://github.com/bryanheinz/keyboard-maestro"
-    exit 1
-fi
-
-if [[ -d /usr/local/keyboard-maestro ]]; then
-    cd /usr/local/keyboard-maestro
-    git pull
-else
-    cd /usr/local/
-    sudo git clone https://github.com/bryanheinz/keyboard-maestro
-    sudo chown -R $theUser /usr/local/keyboard-maestro
+if [[ ! -d $kmDir ]]; then
+    sudo ln -s "$baseDir" "$kmDir"
+    echo "Created Keyboard Maestro symlink from $baseDir to $kmDir."
 fi

017eb507278166c355fd5671f1d1701b878f20f4
bryan heinz <git@bryanheinz.com>
2021-10-29 14:25:48 +0000

    added Monterey, spaced main out a little, reorganized the if block to flow oldest to newest OS.

--- text-manipulation/macOS_installer_renamer.py
+++ text-manipulation/macOS_installer_renamer.py
@@ -10,20 +10,27 @@ from sys import stdin
 
 
 def main(selection):
-    reg = r'Install_macOS_((\d{2})\.\d{1,3}\.?\d{0,3})-(\w+).dmg'
+    reg = r'Install_macOS_((\d{2})\.\d{1,3}\.?\d{0,3})-(\w+)'
     a = re.findall(reg, selection)
+    
     for _ in a:
         full_vers = _[0]
         major_vers = _[1]
         build_vers = _[2]
-        if major_vers == "11":
-            name_vers = "Big Sur"
-        elif '10.15' in full_vers:
-            name_vers = "Catalina"
+        
+        if '10.13' in full_vers:
+            name_vers = 'High Sierra'
         elif '10.14' in full_vers:
-            name_vers = "Mojave"
-        elif '10.13' in full_vers:
-            name_vers = "High Sierra"
+            name_vers = 'Mojave'
+        elif '10.15' in full_vers:
+            name_vers = 'Catalina'
+        elif major_vers == '11':
+            name_vers = 'Big Sur'
+        elif major_vers == '12':
+            name_vers = 'Monterey'
+        else:
+            exit(1)
+        
         new_name = f"macOS {full_vers} {build_vers} {name_vers}"
         print(new_name)
 
